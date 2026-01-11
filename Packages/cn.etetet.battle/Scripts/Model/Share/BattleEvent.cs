f69e79391a006804c2c2c67b80cc59b5b2e1728f
65128792 <65128792@qq.com>
2026-01-11 14:17:42 +0800

    修改编译错误

--- Packages/cn.etetet.battle/Scripts/Model/Share/BattleEvent.cs
+++ Packages/cn.etetet.battle/Scripts/Model/Share/BattleEvent.cs
@@ -1,35 +1,37 @@
 ﻿using System.Collections.Generic;
 
-namespace ET;
-
-/// <summary>
-/// 设置实体状态事件
-/// </summary>
-public struct SetEntityState
+namespace ET
 {
-    public int EntityId;
-    public int state;
+    /// <summary>
+    /// 设置实体状态事件
+    /// </summary>
+    public struct SetEntityState
+    {
+        public int EntityId;
+        public int state;
+    }
+
+    /// <summary>
+    /// 取消实体状态事件
+    /// </summary>
+    public struct UnsetEntityState
+    {
+        public int EntityId;
+        public int state;
+    }
+
+    public struct DamageInfo
+    {
+        public int TargetId;
+        public int Damage;
+        public int SpellResult;
+    }
+
+    public struct EntityCastSpell
+    {
+        public int CasterId;
+        public int SpellId;
+        public List<DamageInfo> DamageInfos;
+    }
 }
 
-/// <summary>
-/// 取消实体状态事件
-/// </summary>
-public struct UnsetEntityState
-{
-    public int EntityId;
-    public int state;
-}
-
-public struct DamageInfo
-{
-    public int TargetId;
-    public int Damage;
-    public int SpellResult;
-}
-
-public struct EntityCastSpell
-{
-    public int CasterId;
-    public int SpellId;
-    public List<DamageInfo> DamageInfos;
-}
